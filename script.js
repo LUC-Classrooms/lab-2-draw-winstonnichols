9cf8ee13dbf122d928f61c3befde59cae109e763
Winston Nichols <157043193+winstonnichols@users.noreply.github.com>
2024-01-28 21:36:20 +0000

    Drew a jellyfish

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
 function setup(){
-  createCanvas(600, 400);
-}
+    createCanvas(600, 400);
+  }
+  function draw(){
+    background(50, 180, 255);
+  ellipse (300, 200, 350, 60, 2 * Math.PI);  // face shape
+   fill(65, 160, 255);  // face color
+   noFill();
+   arc(width/2 + 3, height/2 + 5, 45, 25, 0, PI ); // mouth
+   strokeWeight(10);
+   fill("lightpink");
+   line(125, 200, 125, 400);  // Left Arm
+   line(475, 200, 475, 400); // Right Arm
+   line(300, 230, 100, 430); // middle left
+   line(300, 230, 100, 400) // left left center
+   line(300, 230, 100, 480) // middle middle left
+   line(300, 230, 100, 540); // tentacle
+   line(300, 230, 100, 730); // tentacle
+   line(300, 230, 100, 1750) // tentacle
+   line(300, 230, 500, 400); // tentacle
+   line(300, 230, 475, 405); // tentacle
+   line(300, 230, 475, 450); // tentacle
+   line(300, 230, 475, 515); // tentacle
+   line(300, 230, 475, 600); // tentacle
+   line(300, 230, 475, 800); // tentacle
+   line(300, 230, 475, 2000); // tentacle
+   ellipse (300, 170, 350, 80, 2 * Math.PI);
+   ellipse(width/2 - 35, height/2 - 15, 15); // left eye
+   ellipse(width/2 + 35, height/2 - 15, 15); // right eye
+   arc(width/2 + 3, height/2 + 5, 45, 25, 0, PI ); // mouth
+   point(225, 180, 50, 10); // face dot
+   point(200, 180, 50, 10); // face dot
+   point(180, 160, 50, 10); // face dot
+   point(160, 180, 50, 10); // face dot
+   point(375, 180, 50, 10); // face dot
+   point(400, 180, 50, 10); // face dot
+   point(440, 180, 50, 10); // face dot
+   point(420, 160, 50, 10); // face dot
+   point(300, 150, 50, 10); // face dot
+   point(260, 145, 50, 10); // face dot
+   point(225, 155, 50, 10); // face dot
+   point(340, 145, 50, 10); // face dot
+   point(375, 155, 50, 10); // face dot
+  }
 
-function draw(){
-  background(200);
-  // add your drawing code here ...
+
+
+
+
+
   
-}
+  
